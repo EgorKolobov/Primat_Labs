c55080f39d24d39d795c7bf31b16ce5d1d6baf94
Egor <egorius@MacBook-Air-Egor.local>
2021-03-11 19:43:33 +0300

    добавил метод Брента

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,11 +1,13 @@
 from math import sin, log, sqrt
+from numpy import sign
+from scipy.optimize import minimize_scalar
 
 
 def f(x: float):
     return sin(x)*x**3
 
 
-def dichotomy(a: float, b: float, epsilon: float):
+def dichotomy_method(a: float, b: float, epsilon: float):
     if a > b:
         return None
     if b-a < epsilon:
@@ -14,14 +16,14 @@ def dichotomy(a: float, b: float, epsilon: float):
     x1 = (a+b)/2-sigma
     x2 = (a+b)/2+sigma
     if f(x1) < f(x2):
-        return dichotomy(a, x2, epsilon)
+        return dichotomy_method(a, x2, epsilon)
     elif f(x1) > f(x2):
-        return dichotomy(x1, b, epsilon)
+        return dichotomy_method(x1, b, epsilon)
     else:
-        return dichotomy(x1, x2, epsilon)
+        return dichotomy_method(x1, x2, epsilon)
 
 
-def gold(a: float, b: float, epsilon: float):
+def golden_ratio_method(a: float, b: float, epsilon: float):
     if a > b:
         return None
     if b-a < epsilon:
@@ -31,11 +33,11 @@ def gold(a: float, b: float, epsilon: float):
     x1 = a + ((3-sqrt(5))/2)*(b-a)
     x2 = b - ((3-sqrt(5))/2)*(b-a)
     if f(x1) < f(x2):
-        return gold(a, x2, epsilon)
+        return golden_ratio_method(a, x2, epsilon)
     elif f(x1) > f(x2):
-        return gold(x1, b, epsilon)
+        return golden_ratio_method(x1, b, epsilon)
     else:
-        return gold(x1, x2, epsilon)
+        return golden_ratio_method(x1, x2, epsilon)
 
 
 def fib(n: int):
@@ -98,7 +100,85 @@ def parabola_method(a: float, b: float, epsilon: float):
     return parabola_submethod(a, b, epsilon, f(a), f(b))
 
 
-print("Ответ:", dichotomy(2, 10, 1))
-print("Ответ:", gold(-10, -2, 0.00005))
-print("Ответ:", fibonacci_method(-1, 1, 0.5))
-print("Ответ:", parabola_method(2, 10, 0.5))
+def Brent_combined_method(a: float, c: float, epsilon: float):
+    K = (3-sqrt(5))/2
+    x = (a + c)/2
+    w = x
+    v = x
+    fx = f(x)
+    fw = fx
+    fv = fx
+    d = c-a
+    e = d
+    # epsilon2 = epsilon/2
+    while c-a >= epsilon:
+        # print("c, a:", c, a)
+        # print("x, w, v:", x, w, v)
+        g = e  # e
+        e = d
+        parabola_fit = False
+        if (x != w) and (x != v) and (w != v) and (fx != fw) and (fx != fv) and (fw != fv):
+            print("PARABOLA")
+            f_val = dict()
+            f_val[x] = fx
+            f_val[w] = fw
+            f_val[v] = fv
+            l = sorted([x, w, v])[0]
+            m = sorted([x, w, v])[1]
+            r = sorted([x, w, v])[2]
+            f1, f2, f3 = f_val[l], f_val[m], f_val[r]
+            u = m-((m-l)**2 * (f2-f3) - (m-r)**2 * (f2-f1))/(2*((m-l) * (f2-f3) - (m-r) * (f2-f1)))
+            if u >= a+epsilon and u <= c-epsilon and abs(u-x) < g/2:
+                print("parabola fit")
+                d = abs(u-x)
+                parabola_fit = True
+        # else:
+        if not parabola_fit:
+            # print("not parabola fit")
+            if x < (c-a)/2:
+                u = x + K*(c - x)
+                d = c - x
+            else:
+                u = x - K*(x - a)
+                d = x - a
+            if abs(u-x) < epsilon:
+                # print("< epsilon")
+                u = x + sign(u-x)*epsilon
+        fu = f(u)
+        if fu <= fx:
+            # print("fu<=fx")
+            if u >= x:
+                a = x
+            else:
+                c = x
+            v = w
+            w = x
+            x = u
+            fv = fw
+            fw = fx
+            fx = fu
+        else:
+            if u >= x:
+                # print("u>=x")
+                c = u
+            else:
+                a = u
+            # print("a,c =", a, c)
+            if fu <= fw or w == x:
+                v = w
+                w = u
+                fv = fw
+                fw = fu
+            elif fu <= fv or v == x or v == w:
+                v = u
+                fv = fu
+
+    return c
+
+
+print("Ответ:", dichotomy_method(2, 10, 0.01))
+print("Ответ:", golden_ratio_method(2, 10, 0.01))
+print("Ответ:", fibonacci_method(2, 10, 0.01))
+print("Ответ:", parabola_method(2, 10, 0.01))
+# print("Ответ:", minimize_scalar(f,  bounds=(2, 10), method='bounded').x)
+print("Ответ:", Brent_combined_method(3, 6, 0.01))
